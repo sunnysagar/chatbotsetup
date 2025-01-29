9551183847c9b8dab4d23724927aefc1bb2363a8
Sunny <sunnysagar620@gmail.com>
2025-01-29 13:08:39 +0530

    commit init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './Style.css'
+
+const App = () => {
+
+  return(
+    <div>
+      <h1>React + Vite</h1>
+    </div>
+  );
+}
+
+export default App
