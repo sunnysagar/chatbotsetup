a08157b3a6d4f3cff52dac786ddf7ba6bc948f86
Sunny <sunnysagar620@gmail.com>
2025-01-31 19:52:06 +0530

    commit the change in server

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors({ origin: "https://ai-c-bot.netlify.app/" }));
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB connection error:", err));
 
